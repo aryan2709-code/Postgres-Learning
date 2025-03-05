c5057ef6ec4adc4c4afe82290b3e334ac0ba9291
Aryan Sharma <bhardwaj7a@gmail.com>
2025-03-05 20:12:48 +0530

    Removed exposed password

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Client  } from "pg";
 
 // Creating an instance of an postgres client
 // The Client constructor either accepts a client object or an string 
-const pgClient = new Client("postgresql://neondb_owner:npg_0xEhOM8VAfzm@ep-autumn-frog-a5knrug8-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
+const pgClient = new Client("postgresql://neondb_owner:yourpassword@ep-autumn-frog-a5knrug8-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
 
 async function main() {
     await pgClient.connect();
