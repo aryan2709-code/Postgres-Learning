3532b9bab52987326100922b899ce2a246d93153
Aryan <bhardwaj7a@gmail.com>
2025-03-05 21:20:43 +0530

    Added a simple sign up logic along with sql injection scope

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import { Client  } from "pg";
-
+import express from "express";
+const app = express();
+app.use(express.json())
 // Creating an instance of an postgres client
 // The Client constructor either accepts a client object or an string 
 const pgClient = new Client("postgresql://neondb_owner:npg_0xEhOM8VAfzm@ep-autumn-frog-a5knrug8-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
 
-async function main() {
-    await pgClient.connect();
-    const response = await pgClient.query("SELECT * FROM users;")
-    console.log(response.rows);
-}
+pgClient.connect()
 
-main()
+app.post("/signup" , async(req , res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    try{
+          // SQL Injection
+        const insertQuery = `INSERT INTO users (username , email , password) VALUES ($1 , $2 , $3); `
+    const response = await pgClient.query(insertQuery , [username , email , password] );
+    res.json({
+        message : "You have signed up"
+    })
+    }catch(e){
+        console.log(e);
+        res.json({
+            message : "An error occured"
+        })
+    }
+
+})
+
+app.listen(3000);
