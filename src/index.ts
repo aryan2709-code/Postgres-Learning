6a3095175208bdd80310e511a25cc27c6660e6ff
Aryan <bhardwaj7a@gmail.com>
2025-03-05 20:05:36 +0530

    Establish a connection with postgre sql database neon db through node js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import { Client  } from "pg";
+
+// Creating an instance of an postgres client
+// The Client constructor either accepts a client object or an string 
+const pgClient = new Client("postgresql://neondb_owner:npg_0xEhOM8VAfzm@ep-autumn-frog-a5knrug8-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
+
+async function main() {
+    await pgClient.connect();
+    const response = await pgClient.query("SELECT * FROM users;")
+    console.log(response.rows);
+}
+
+main()
