bd39927df50a5e94c4252908537567a8cb28cc49
Aryan <bhardwaj7a@gmail.com>
2025-03-11 21:11:21 +0530

    Use Join for querying

--- src/index.ts
+++ src/index.ts
@@ -62,4 +62,15 @@ res.json({
 
 })
 
+app.get("/better-metadata" , async(req , res) => {
+    const id = req.query.id;
+    const query = `SELECT users.id , users.username , users.email , addresses.city, addresses.country , addresses.street , addresses.pincode
+     FROM users JOIN addresses ON users.id = addresses.user_id WHERE users.id = $1 ;`
+
+    const response = await pgClient.query(query , [id]);
+    res.json({
+        response : response.rows
+    })
+})
+
 app.listen(3000);
